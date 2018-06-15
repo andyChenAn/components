b491274e21d0136ceb857ab7516a0f9c0ceb7581
andy <160149808@qq.com>
2018-06-15 14:33:54 +0800

    feat:修改回到顶部代码

--- static/util/util.js
+++ static/util/util.js
@@ -1,32 +1,44 @@
-let _ = {
-    query : function (attr) {
-        if (attr.indexOf('#') > -1) {
-            return document.querySelector(attr);
-        } else {
-            return document.querySelectorAll(attr);
-        }
-    },
-    extend : function (...args) {
-        if (args.length == 0) {
-            throw new Error('参数不能为空');
-        };
-        args.map(function (item) {
-            if (typeof item !== 'object' && item.toString() !== '[object Object]') {
-                throw new Error('传入的参数必须是对象字面量');
+(function () {
+    var root = (typeof self == 'object' && self.self == self && self)
+            || (typeof global == 'object' && global.global == global && global)
+            || this || {};
+    var _ = {
+        query : function (attr) {
+            if (attr.indexOf('#') > -1) {
+                return document.querySelector(attr);
+            } else {
+                return document.querySelectorAll(attr);
             }
-        });
-        if (args.length == 1) {
-            return args[0];
-        };
-        let target = args[0];
-        let otherArgs = args.slice(1);
-        for (let i = 0 ; i < otherArgs.length ; i++) {
-            let origin = otherArgs[i];
-            for (let key in origin) {
-                target[key] = origin[key];
+        },
+        extend : function (...args) {
+            if (args.length == 0) {
+                throw new Error('参数不能为空');
+            };
+            args.map(function (item) {
+                if (typeof item !== 'object' && item.toString() !== '[object Object]') {
+                    throw new Error('传入的参数必须是对象字面量');
+                }
+            });
+            if (args.length == 1) {
+                return args[0];
+            };
+            let target = args[0];
+            let otherArgs = args.slice(1);
+            for (let i = 0 ; i < otherArgs.length ; i++) {
+                let origin = otherArgs[i];
+                for (let key in origin) {
+                    target[key] = origin[key];
+                }
             }
+            return target;
         }
-        return target;
+    };
+    if (typeof exports != 'undefined' && !exports.nodeType) {
+        if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+            exports = module.exports = _;
+        }
+        exports._ = _;
+    } else {
+        root._ = _;
     }
-}
-
+})();
