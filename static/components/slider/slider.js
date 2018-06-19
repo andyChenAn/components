df05a0f6bded0033400e4e3553f444d5ebfedc68
andy <160149808@qq.com>
2018-06-20 06:41:51 +0800

    feat:添加图片轮播js

--- static/components/slider/slider.js
+++ static/components/slider/slider.js
@@ -1,11 +1,29 @@
-function Slider (element , options) {
-    if (!this instanceof Slider) {
-        return new Slider(element , options);
+;(function () {
+    var root = (typeof self == 'object' && self.self == self && self) ||
+                (typeof global == 'object' && global.global == global && global) ||
+                this || {};
+    function Slider (element , options) {
+        if (!(this instanceof Slider)) {
+            return new Slider(element , options);
+        };
+        var defaults = {
+    
+        };
+        this.element = element;
+        this.options = _.extend({} , defaults , options);
+        this.init();
     };
-    var defaults = {
-
+    var proto = Slider.prototype;
+    proto.init = function () {
+        // 计算样式
+        this.computeStyle();
     };
-    this.element = element;
-    this.options = _.extend({} , defaults , options);
-
-};
+    proto.computeStyle = function () {
+        var winW = window.innerWidth;
+        var oneImage = document.getElementsByClassName('slider-image')[0];
+        oneImage.onload = function () {
+            console.log(this.height);
+        }
+    }
+    root.Slider = Slider;
+})();
