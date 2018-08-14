9e0a0924ecfba53fef0bd2c4c09052609258ec30
andy <160149808@qq.com>
2018-08-14 23:51:12 +0800

    feat:添加弹框组件js逻辑

--- static/components/dialog/dialog.js
+++ static/components/dialog/dialog.js
@@ -7,7 +7,12 @@
             return new Dialog(element , options);
         }
         var defaults = {
-            title : '提示'
+            title : '提示',
+            titleShow : true,
+            style : 'default',
+            type : 'alert',
+            content : '',
+            closeBtnShow : false
         };
         this.target = element;
         this.settings = _.extend({} , defaults , options);
@@ -15,16 +20,46 @@
         this.init();
     };
     Dialog.prototype.init = function () {
-        // 初始化弹框DOM
-        this.renderDOM();
         // 绑定事件
         this.target.addEventListener('click' , this.handleClick.bind(this));
     };
     Dialog.prototype.handleClick = function (e) {
-        
+        this.renderDOM();
     };
     Dialog.prototype.renderDOM = function () {
-        
+        this.dialogBox = document.createElement('div');
+        var html = '<div id="dialog" class="dialog dialog-show">'+
+                        '<div class="dialog-mask" id="dialog-mask"></div>'+
+                        '<div id="dialog-content" class="dialog-content">'+
+                            '<div id="dialog-content-header" class="dialog-content-header">'+ this.settings.title +'</div>'+
+                            '<div id="dialog-content-body" class="dialog-content-body">'+ this.settings.content +'</div>'+
+                            '<div id="dialog-content-footer" class="dialog-content-footer">'+
+                                '<button class="dialog-btn">确定</button>'+
+                            '</div>'+
+                            '<div id="dialog-close-btn" class="dialog-close-btn"></div>'+
+                        '</div>'+
+                    '</div>';
+        this.dialogBox.innerHTML = html;
+        document.body.appendChild(this.dialogBox);
+        // 获取相关DOM节点
+        this.getDOM();
+        // 绑定相关DOM节点的事件处理函数
+        this.bindEvents();
+    };
+    Dialog.prototype.getDOM = function () {
+        this.dialog = document.getElementById('dialog');
+        this.btns = document.getElementsByClassName('dialog-btn');
+    };
+    Dialog.prototype.bindEvents = function () {
+        for (var i = 0 , len = this.btns.length ; i < len ; i++) {
+            this.btns[i].addEventListener('click' , this.handleBtnClick.bind(this));
+        }
+    };
+    Dialog.prototype.handleBtnClick = function (e) {
+        this.dialogBox.parentNode.removeChild(this.dialogBox);
+        for (var i = 0 , len = this.btns.length ; i < len ; i++) {
+            this.btns[i].removeEventListener('click');
+        }
     };
     root.Dialog = Dialog;
 })();
