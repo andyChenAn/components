4e179512f8bd15b6c4c65a1620e62bfd078276ee
andy <160149808@qq.com>
2018-08-15 18:09:22 +0800

    feat:修改弹框组件

--- static/components/dialog/dialog.js
+++ static/components/dialog/dialog.js
@@ -12,6 +12,7 @@
             style : 'default',
             type : 'alert',
             content : '',
+            
             closeBtnShow : false
         };
         this.target = element;
@@ -28,7 +29,8 @@
     };
     Dialog.prototype.renderDOM = function () {
         this.dialogBox = document.createElement('div');
-        var html = '<div id="dialog" class="dialog dialog-show">'+
+        var html = '';
+        var alertHtml = '<div id="dialog" class="dialog dialog-show">'+
                         '<div class="dialog-mask" id="dialog-mask"></div>'+
                         '<div id="dialog-content" class="dialog-content">'+
                             '<div id="dialog-content-header" class="dialog-content-header">'+ this.settings.title +'</div>'+
@@ -36,19 +38,47 @@
                             '<div id="dialog-content-footer" class="dialog-content-footer">'+
                                 '<button class="dialog-btn">确定</button>'+
                             '</div>'+
-                            '<div id="dialog-close-btn" class="dialog-close-btn"></div>'+
+                            '<div id="dialog-close-btn" class="dialog-close-btn">+</div>'+
                         '</div>'+
                     '</div>';
+        var confirmHtml = '<div id="dialog" class="dialog dialog-show">'+
+                            '<div class="dialog-mask" id="dialog-mask"></div>'+
+                            '<div id="dialog-content" class="dialog-content">'+
+                                '<div id="dialog-content-header" class="dialog-content-header">'+ this.settings.title +'</div>'+
+                                '<div id="dialog-content-body" class="dialog-content-body">'+ this.settings.content +'</div>'+
+                                '<div id="dialog-content-footer" class="dialog-content-footer">'+
+                                    '<button class="dialog-btn">取消</button>'+
+                                    '<button class="dialog-btn">确定</button>'+
+                                '</div>'+
+                                '<div id="dialog-close-btn" class="dialog-close-btn">+</div>'+
+                            '</div>'+
+                        '</div>';
+        switch (this.settings.type) {
+            case 'alert':
+                html = alertHtml;
+            break;
+            case 'confirm':
+                html = confirmHtml;
+            break;
+        }
         this.dialogBox.innerHTML = html;
         document.body.appendChild(this.dialogBox);
         // 获取相关DOM节点
         this.getDOM();
         // 绑定相关DOM节点的事件处理函数
         this.bindEvents();
+        // 根据传入的参数来确定最终展示在页面上的DOM
+        this.checkDOM();
+    };
+    Dialog.prototype.checkDOM = function () {
+        if (!this.settings.title) {
+            this.dialogTitle.parentNode.removeChild(this.dialogTitle);
+        }
     };
     Dialog.prototype.getDOM = function () {
         this.dialog = document.getElementById('dialog');
         this.btns = document.getElementsByClassName('dialog-btn');
+        this.dialogTitle = document.getElementById('dialog-content-header');
     };
     Dialog.prototype.bindEvents = function () {
         for (var i = 0 , len = this.btns.length ; i < len ; i++) {
